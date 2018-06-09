ca862dd464d4d2a7fcd38691d8962cd59fe2dc38
Ken Lin <kenlin@kkbox.com>
2018-06-09 09:56:58 +0800

    Add all test case

--- tests/TennisTest.php
+++ tests/TennisTest.php
@@ -26,6 +26,19 @@ final class TennisTest extends TestCase
     {
         return [
             [0, 0, 'Love All'],
+//            [1, 0, 'Fifteen Love'],
+//            [2, 0, 'Thirty Love'],
+//            [3, 0, 'Forty Love'],
+//            [0, 1, 'Love Fifteen'],
+//            [0, 2, 'Love Thirty'],
+//            [1, 1, 'Fifteen All'],
+//            [2, 2, 'Thirty All'],
+//            [3, 3, 'Deuce'],
+//            [4, 3, 'Joey Adv'],
+//            [3, 4, 'Tom Adv'],
+//            [4, 4, 'Deuce'],
+//            [5, 3, 'Joey Win'],
+//            [3, 5, 'Tom Win'],
         ];
     }
 
